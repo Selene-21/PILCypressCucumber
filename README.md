513a5d7cc9eddf5eef9f107f7cfc8bd1c226935e
Selene-21 <jadelu2919@gmail.com>
2023-10-02 18:05:32 -0300

    se agregaron modificaciones en el package y se agregaron nuevas configuraciones en el config.js

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # PILCypressCucumber
 
+# INDICE
+
 ## Instalacion
+
 1. realizar un npm install
 2. descargar las dependencias "..." y "..."
 3. realizar la configuracion.
 
 ## Configuracion de Cucumber
+
+## Configuracion del ENV en caso de no usar .dotenv
+
+1. ingresar a cypress.config.js y agregar el siguiente fragmento de código con los e-mail address y pass correspondientes a cada ENVIROMENT:
+
+//Template.env
+
+    E2E_TESTING_APLICATION_USERNAME_ACCESS:
+    "xxxxxxx@gamil.com",
+    E2E_TESTING_APLICATION_PASSWORD_ACCESS_DEV:
+    "ZZZZZZZZ",
+    E2E_TESTING_APLICATION_PASSWORD_ACCESS_TST:
+    "LALALALALA",
--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,33 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/Features/**/**.feature",
     async setupNodeEvents(on, config) {
-      const bundler = createBundler({ 
+      const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
-      })
-      on('file:preprocessor', bundler);
+      });
+      on("file:preprocessor", bundler);
       await addCucumberPreprocessorPlugin(on, config);
       return config;
+    },
+
+    env: {
+      //Template.env
+      E2E_TESTING_APLICATION_USERNAME_ACCESS: "xxxxxxx@gamil.com",
+      E2E_TESTING_APLICATION_PASSWORD_ACCESS_DEV: "ZZZZZZZZ",
+      E2E_TESTING_APLICATION_PASSWORD_ACCESS_TST: "LALALALALA",
+
+      //URLs
+      DEV_URL: "",
+      TST_URL: "",
+      INT_URL: "",
+      PROD_URL: "",
     },
   },
 });
