2061ba94d99623ca118e0018bdcb9ae8356cd108
Selene-21 <jadelu2919@gmail.com>
2023-10-11 17:05:52 -0300

    se modifico el step definitions de eden

--- cypress/e2e/Step_definitions/eden.js
+++ cypress/e2e/Step_definitions/eden.js
@@ -10,3 +10,24 @@ When(`escribe {string} en el campo buscador`, (words) => {
 When(`hace click en la sugerencia {String}`, (suggestionTxt) => {
   edenHome.getSearchSugestions().contains(suggestionTxt).click();
 });
+
+Then(`se verifica que el titulo sea {string}`, () => {});
+
+When(`la fecha es {string} de {string} a las {string} Hs`, () => {});
+
+Then(`se verifican los suiguientes datos del evento`, (dataTable) => {
+  dataTable = dataTable.rawTable();
+
+  cy.log(DataTable);
+  cy.log(DataTable[0]);
+  cy.log(DataTable[0][0]);
+  cy.log(DataTable[0][1]);
+
+  cy.log(DataTable[1]);
+  cy.log(DataTable[1][0]);
+  cy.log(DataTable[1][1]);
+
+  cy.log(DataTable[2]);
+  cy.log(DataTable[2][0]);
+  cy.log(DataTable[2][1]);
+});
